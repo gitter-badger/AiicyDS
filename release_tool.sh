1135d404cda7f4fd656453a076399575426b971e
sndnvaps <sndnvaps@gmail.com>
2017-02-04 20:08:47 +0800

    add start.sh && stop.sh && LICENSE to zip file
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- release_tool.sh
+++ release_tool.sh
@@ -8,7 +8,7 @@ NOW=$(date -u '+%Y%m%d%I%M%S')
 OS=$(uname -s)
 OS_TYPE=$(uname -m)
 
-ZIP_FILE=AiicyDS-${OS}-${OS_TYPE}
+ZIP_FILE_PREFIX=AiicyDS-${OS}-${OS_TYPE}
 
 if [ ! -f release_tool.sh ]; then
     echo 'release_tool.sh must be run within its container folder' 1>&2
@@ -18,6 +18,7 @@ fi
 # Create release dir, if it not exists
 if [ ! -d $RELEASE_ROOT ]; then
 	mkdir $RELEASE_ROOT
+	mkdir  -p $RELEASE_ROOT/license
 fi
 
 # test bin/AiicyDS exists or not
@@ -32,23 +33,26 @@ function CopyFiles() {
 	cp -rp ./config/ ${RELEASE_ROOT}/
 	cp -rp ./static/ ${RELEASE_ROOT}/
 	cp -rp ./template/ ${RELEASE_ROOT}/
+	cp start.sh ${RELEASE_ROOT}/
+	cp stop.sh ${RELEASE_ROOT}/
+	cp LICENSE ${RELEASE_ROOT}/license/
 }
 
 # pack all the file in a zip file
 function Pack() {
-	cd ${RELEASE_ROOT} && zip -r ../${ZIP_FILE}.zip .
+	cd ${RELEASE_ROOT} && zip -r ../${ZIP_FILE_PREFIX}.zip .
 }
 
 # print the zip file name
 function PrintZipName() {
-	echo ${ZIP_FILE}.zip
+	echo ${ZIP_FILE_PREFIX}.zip
 }
 
 # write the md5sum info to a file
 function WriteMD5SUM() {
 	cd ../
-	echo "generate the md5sum for " ${ZIP_FILE}.zip
-	md5sum ${ZIP_FILE}.zip >> ${ZIP_FILE}.md5sum
+	echo "generate the md5sum for " ${ZIP_FILE_PREFIX}.zip
+	md5sum ${ZIP_FILE_PREFIX}.zip >> ${ZIP_FILE_PREFIX}.md5sum
 }
 
 CopyFiles
